99362f869b494cde2fb06bcca8af75e97fdf2cd3
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2025-06-17 16:59:09 +0100

    Updated component styles

--- components/score.jsx
+++ components/score.jsx
@@ -1,13 +1,13 @@
 export default function Score() {
     return (
-        <div className="score-container">
-            <p style={{ color: "hsla(0, 0%, 100%,0.75)" }}>Your Result</p>
-            <div className="score-background">
+        <div className="score">
+            <p style={{ color: "hsl(241, 100%, 89%)", fontWeight: "700" }}>Your Result</p>
+            <div className="score__background">
                 <h1 style={{ fontSize: "60px" }}>76</h1>
-                <p style={{ color: "hsla(0, 0%, 100%,0.5)" }}>of 100</p>
+                <p style={{ color: "hsl(241, 100%, 89%)" }}>of 100</p>
             </div>
-            <h2>Great</h2>
-            <p className="score-info" style={{ color: "hsla(0, 0%, 100%,0.75)" }}>
+            <h1 className="score__compliment">Great</h1>
+            <p className="score__info" style={{ color: "hsl(241, 100%, 89%)" }}>
                 You scored higher than 65% of the people who have taken these tests.
             </p>
         </div>
