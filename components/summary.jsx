eb56ad6359b42827a3ec5ad5082d95e4b642e6c8
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2025-06-17 17:01:46 +0100

    Modified root component

--- components/summary.jsx
+++ components/summary.jsx
@@ -7,35 +7,37 @@ import Skill from "./skill";
 export default function Summary() {
     return (
         <div className="summary">
-            <h2 className="summary__heading">Summary</h2>
-            <Skill
-                skillBackground="skill reaction-background"
-                icon={reactionIcon}
-                color="hsl(0, 100%, 67%)"
-                skill="Reaction"
-                score={80}
-            />
-            <Skill
-                skillBackground="skill memory-background"
-                icon={memoryIcon}
-                color="hsl(39, 100%, 56%)"
-                skill="Memory"
-                score={92}
-            />
-            <Skill
-                skillBackground="skill verbal-background"
-                icon={verbalIcon}
-                color="hsl(166, 100%, 37%)"
-                skill="Verbal"
-                score={61}
-            />
-            <Skill
-                skillBackground="skill visual-background"
-                icon={visualIcon}
-                color="hsl(234, 85%, 45%)"
-                skill="Visual"
-                score={73}
-            />
+            <p className="summary__heading">Summary</p>
+            <div className="skills">
+                <Skill
+                    skillBackground="skill reaction-background"
+                    icon={reactionIcon}
+                    color="hsl(0, 100%, 67%)"
+                    skill="Reaction"
+                    score={80}
+                />
+                <Skill
+                    skillBackground="skill memory-background"
+                    icon={memoryIcon}
+                    color="hsl(39, 100%, 56%)"
+                    skill="Memory"
+                    score={92}
+                />
+                <Skill
+                    skillBackground="skill verbal-background"
+                    icon={verbalIcon}
+                    color="hsl(166, 100%, 37%)"
+                    skill="Verbal"
+                    score={61}
+                />
+                <Skill
+                    skillBackground="skill visual-background"
+                    icon={visualIcon}
+                    color="hsl(234, 85%, 45%)"
+                    skill="Visual"
+                    score={73}
+                />
+            </div>
             <button className="summary__button">Continue</button>
         </div>
     );
--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Score from "../components/score";
+import Results from "../components/results";
 
 function App() {
   return (
     <>
-      <Score />
+      <Results />
     </>
   );
 }
