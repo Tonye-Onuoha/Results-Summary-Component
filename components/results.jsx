f61b6ea9ef58bda6c80e935a2ac874df54bf04dc
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2025-06-17 16:57:41 +0100

    Created "Results" component

---
+++ components/results.jsx
@@ -0,0 +1,11 @@
+import Score from "./score";
+import Summary from "./summary";
+
+export default function Results() {
+    return (
+        <div className="result-summary">
+            <Score />
+            <Summary />
+        </div>
+    );
+}
